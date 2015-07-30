710fe68bb0673a014fcbcc0890625ae2183a3988
Kaveh Ghahremani <kaveh.ghahremani@gmail.com>
2015-07-30 17:53:39 -0400

    Start on report service

---
+++ death-star/src/test/java/io/enforcer/deathstar/services/ReportServiceTest.java
@@ -0,0 +1,28 @@
+package io.enforcer.deathstar.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kavehg on 7/30/2015.
+ */
+public class ReportServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testAddReport() throws Exception {
+
+    }
+}
